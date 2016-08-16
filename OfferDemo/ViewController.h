f2b80c3e61b3e7422b648d05e1da345041558462
xuewugoujie <2693350848@qq.com>
2016-08-16 11:20:27 +0800

    Initial Commit

---
+++ OfferDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OfferDemo
+//
+//  Created by xuewuguojie on 16/8/16.
+//  Copyright © 2016年 xuewuguojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OfferDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OfferDemo
+//
+//  Created by xuewuguojie on 16/8/16.
+//  Copyright © 2016年 xuewuguojie. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
